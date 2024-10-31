111d80b2f9dce14ea83e740141a8c7c2f62c6c7a
thesoftninja <89429670+thesoftninja@users.noreply.github.com>
2024-11-01 01:04:26 +0200

    mobile menu & media queries

--- LUT_coursework/All Files/script.js
+++ LUT_coursework/All Files/script.js
@@ -17,10 +17,35 @@ document.addEventListener('DOMContentLoaded', () => {
         //Toggle icon
         icon.classList.toggle('fa-plus');
         icon.classList.toggle('fa-minus');
+
+        // Toggle visibility of body
+        groupBody.classList.toggle('open');
+
+        // Close other open FAQ bodies
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+        otherGroups.forEach((otherGroup) => {
+            if (otherGroup !== group) {
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+        })
     });
 });
 
 
+// Mobile Menu
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+})
 
 
 
